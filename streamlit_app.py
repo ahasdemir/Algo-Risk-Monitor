564647d0e553787f3406296a1e4b69d668e06bc6
Ahmet Hasdemir <ahmet@hasdemir.me>
2025-12-28 10:41:40 +0300

    Update streamlit_app.py: Change layout configuration to centered for improved UI

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.set_page_config(
     page_title="The Algo Risk Monitor",
     page_icon="📈",
-    layout="wide",
+    layout="centered",
     initial_sidebar_state="expanded",
 )
 
